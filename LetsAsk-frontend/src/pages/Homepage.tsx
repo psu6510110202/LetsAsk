64cba8795aa77efccec4b564045a527e0c75fead
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-01-28 23:35:03 +0700

    Create Navbar component

---
+++ LetsAsk-frontend/src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from "../components/Navbar";
+
+const Homepage = () => {
+    return (
+        <div>
+            <NavigationBar/>
+            <div>This is home</div>
+        </div>      
+    )
+}
+
+export default Homepage;
