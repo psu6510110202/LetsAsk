2efe338be4f682d314e2326ac5d54c5ac2aedae8
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-01-30 00:02:04 +0700

    Make Always scrolltoTop

--- LetsAsk-frontend/src/App.tsx
+++ LetsAsk-frontend/src/App.tsx
@@ -3,12 +3,14 @@ import Homepage from "./pages/Homepage"
 import Loginpage from "./pages/Loginpage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Articlepage from "./pages/Articlepage";
+import { ScrollToTop } from "./Helper";
 
 
 function App() {
 
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Routes>
         <Route index element={<Homepage/>}/>
         <Route path='/article/:id' element={<Articlepage/>}/>
