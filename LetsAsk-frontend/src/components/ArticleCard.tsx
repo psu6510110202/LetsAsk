5562b527beadf9afcb312dce43b15ed7dbdd2c46
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-01-29 23:54:14 +0700

    Change data source Articlepage

--- LetsAsk-frontend/src/components/ArticleCard.tsx
+++ LetsAsk-frontend/src/components/ArticleCard.tsx
@@ -6,59 +6,73 @@ import ChatIcon from '@mui/icons-material/Chat';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import FlagIcon from '@mui/icons-material/Flag';
+import Articles from '../models/Articles';
+import conf from "../conf";
 
+interface Props {
+    ArticleData : Articles 
+}
 
+function ArticleCard(props : Props) {
+    const item = props.ArticleData.attributes
+    const avatar = `${conf.apiPrefix}${item.AvatarCreator}`
 
-function ArticleCard() {
-  return (
-    <Card style={{ width: '100rem', borderRadius: "40px" , backgroundColor: "#282727", color: "white", marginTop: "10px"}}>
-        <Card.Body>
-        <Card.Title>
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-                <h3>I don’t know why my cat is so cute, I can’t take my eyes off!</h3>
-                <Button>
-                    <FlagIcon style={{ fontSize: '2rem', color: '#F32222' }} />
-                </Button>
-            </div>
-        </Card.Title>
+    const dateTime = new Date(item.createdAt);
 
-        <Card.Text>
-            <Container fluid="md">
-                <Row>
-                    <Col>
-                        <Button variant="contained" style={{backgroundColor: '#6B1212', borderRadius: '60px', marginRight: '10px'}}>Pet</Button>
-                        <Button variant="contained" style={{backgroundColor: '#6B1212', borderRadius: '60px', marginRight: '10px'}}>Life problem</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </Card.Text>
-        <Card.Text>
-            <p>
-            &nbsp;&nbsp;&nbsp;&nbsp;I can't help but marvel at the adorable nature of my cat. It's as if there's an enchanting spell cast upon him that makes it impossible for me to look away. Every time he gracefully moves or playfully pounces, my heart melts. The inexplicable charm that radiates from him captivates my attention, leaving me in awe. His fur, soft and silky, invites endless cuddles, and those big, expressive eyes seem to hold a universe of charm. I find myself constantly drawn to his cute antics, whether he's chasing his tail, batting at a feather, 
-            or simply lounging in the sun. It's a joy to witness such innocence and playfulness.
-            </p>
-        </Card.Text>
-        <Card.Text>
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <Avatar
-                    alt="Remy Sharp"
-                    src="/avatar1.jpeg"
-                    sx={{ width: 56, height: 56, marginRight: "10px" }}
-                />
-                <h5 style={{color: "#F32222"}}>User: @Ilovemycat &nbsp;&nbsp; 28/10/2024 &nbsp;&nbsp; 22:30</h5>
-                <div style={{ marginLeft: 'auto' }}>
+    const date = dateTime.toDateString();
+    const time = dateTime.toTimeString().split(' ')[0];
+
+    const tagsList = item.Tags.split(',').map(item => item.trim());
+    console.log(avatar)
+    return (
+        <Card style={{ width: '100rem', borderRadius: "40px" , backgroundColor: "#282727", color: "white", marginTop: "10px"}}>
+            <Card.Body>
+            <Card.Title>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                    <h3>{item.Topic}</h3>
+                    <Button>
+                        <FlagIcon style={{ fontSize: '2rem', color: '#F32222' }} />
+                    </Button>
+                </div>
+            </Card.Title>
+
+            <Card.Text>
+                <Container fluid="md">
                     <Row>
-                        <Col style={{ display: "flex", alignItems: "center" }}>
-                            <ChatIcon style={{ fontSize: '3rem', color: '#F32222' }} />
-                            <h3 style={{ marginLeft: '5px' }}>5</h3>
+                        <Col>
+                            {tagsList.map((item) => 
+                                <Button variant="contained" style={{backgroundColor: '#6B1212', borderRadius: '60px', marginRight: '10px'}}>{item}</Button>
+                            )}
                         </Col>
                     </Row>
+                </Container>
+            </Card.Text>
+            <Card.Text>
+                <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;{item.Description}
+                </p>
+            </Card.Text>
+            <Card.Text>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <Avatar
+                        alt="Remy Sharp"
+                        src={avatar}
+                        sx={{ width: 56, height: 56, marginRight: "10px" }}
+                    />
+                    <h5 style={{color: "#F32222"}}>User: {item.Creator} &nbsp;&nbsp; {date} &nbsp;&nbsp; {time}</h5>
+                    <div style={{ marginLeft: 'auto' }}>
+                        <Row>
+                            <Col style={{ display: "flex", alignItems: "center" }}>
+                                <ChatIcon style={{ fontSize: '3rem', color: '#F32222' }} />
+                                <h3 style={{ marginLeft: '5px' }}>5</h3>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
-            </div>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+            </Card.Text>
+        </Card.Body>
+        </Card>
+    );
 }
 
 export default ArticleCard;
--- LetsAsk-frontend/src/pages/Articlepage.tsx
+++ LetsAsk-frontend/src/pages/Articlepage.tsx
@@ -4,15 +4,39 @@ import NavigationBar from "../components/Navbar";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import WriteCommentCard from "../components/WriteCommentCard";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Articles from "../models/Articles";
+import Repo from "../repositories"
 
 
 const Articlepage = () => {
+    const [articleData, setArticleData] = useState<Articles[]>([])
+    const params = useParams();
+
+    const fetchData = async () => {
+        const resp = await Repo.Articledata.getArticleById(params.id as string)
+        if (resp) {
+            if (Array.isArray(resp)) {
+                setArticleData(resp);
+            } else {
+                setArticleData([resp]);
+            }
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    },[params.id])
+
+    const item: Articles | null = articleData.length > 0 ? articleData[0] : null;
+
     return (
         <div>
             <NavigationBar/>
             <Container>
                 <Row>
-                    <ArticleCard/>
+                {item && <ArticleCard ArticleData={item}/>}
                 </Row>
             </Container>
             <div style={{alignItems: 'center', display: "flex", justifyContent: 'center'}}>
