e5ab53d9371bb772e1dc38005fd8016146c6c2b3
s6510110166 <6510110166@email.psu.ac.th>
2024-01-30 21:22:50 +0700

    Edit Writecard Component (login)

--- LetsAsk-frontend/src/components/WriteCommentCard.tsx
+++ LetsAsk-frontend/src/components/WriteCommentCard.tsx
@@ -1,46 +1,42 @@
 import Card from 'react-bootstrap/Card';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login'
 import { userData } from '../Helper';
 import conf from '../conf';
 import Form from 'react-bootstrap/Form';
+
 function WriteCommentCard() {
     const user = userData()
     const avatar = `${conf.apiPrefix}${user.avatar}`
 
     return (
-        <>{!user.jwt && 
-            <Card style={{ width: '100rem', height: '10rem', borderRadius: "40px", backgroundColor: "#282727", color: "white", marginTop: "10px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <Card.Body>
-                    <Card.Text style={{ textAlign: 'center', marginTop: '5%' }}>
-                    <div>
-                        <h4>You need to login before commenting</h4>
-                        <Button variant='contained' style={{ backgroundColor: '#8A0A0A', borderRadius: '20px' }} href='/login'>
-                            <LoginIcon style={{marginRight: "5px"}}/>
-                            Login / Sign up
-                        </Button>
-                    </div>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        }
-        {user.jwt && 
-            <Card style={{ width: '100rem', height: '10rem', borderRadius: "40px", backgroundColor: "#282727", color: "white", marginTop: "10px"}}>
-                <Card.Body>
-                    {!user.jwt &&
+        <>
+            {!user.jwt &&
+                <Card style={{ width: '100rem', height: '10rem', borderRadius: "40px", backgroundColor: "#282727", color: "white", marginTop: "10px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <Card.Body>
                         <Card.Text style={{ textAlign: 'center', marginTop: '5%' }}>
-                        <div>
-                            <h4>You need to login before commenting</h4>
-                            <Button variant='contained' style={{ backgroundColor: '#8A0A0A', borderRadius: '20px' }} href='/login'>
-                                <LoginIcon style={{marginRight: "5px"}}/>
-                                Login / Sign up
-                            </Button>
-                        </div>
+                            <div>
+                                <h4>You need to login before commenting</h4>
+                                <Button variant='contained' style={{ backgroundColor: '#8A0A0A', borderRadius: '20px' }} href='/login'>
+                                    <LoginIcon style={{ marginRight: "5px" }} />
+                                    Login / Sign up
+                                </Button>
+                            </div>
                         </Card.Text>
-                    }
-                </Card.Body>
-            </Card>
-        }
+                    </Card.Body>
+                </Card>
+            }
+            {user.jwt &&
+                <Card style={{ width: '100rem', height: '15rem', borderRadius: "20px", backgroundColor: "#282727", color: "white", marginTop: "10px" }}>
+                    <Typography variant="h5" style={{ marginTop: '10px', marginBottom: '10px', marginLeft: '15px' }}>
+                        Your Comment
+                    </Typography>
+                    <Form.Control as="textarea" rows={5} placeholder="Write your comment here" style={{ marginLeft: '3px'}} />
+                    <Button variant="contained" style={{ backgroundColor: '#8A0A0A', borderRadius: '20px', marginTop: '10px', alignSelf: 'flex-end' }}>
+                        Comment
+                    </Button>
+                </Card>
+            }
         </>
     )
 
