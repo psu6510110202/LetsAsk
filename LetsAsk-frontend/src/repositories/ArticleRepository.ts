538f0db4ab9dc437baf79e394030b13f71553479
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-01-31 21:56:48 +0700

    Add createArticle to Repo

--- LetsAsk-frontend/src/repositories/ArticleRepository.ts
+++ LetsAsk-frontend/src/repositories/ArticleRepository.ts
@@ -1,8 +1,9 @@
 import Articles from "../models/Articles";
 import { IRepository } from "./IRepositories";
 import conf from '../conf'
+import PostArticle from "../models/PostArticles";
 
-export class ArticleRepository implements IRepository<Articles> {
+export class ArticleRepository implements IRepository<Articles | PostArticle> {
     urlPrefix = `${conf.apiPrefix}/api/post-contents`
 
     async getAll(): Promise<Articles[] | null> {
@@ -21,5 +22,19 @@ export class ArticleRepository implements IRepository<Articles> {
         const resp = await fetch(`${this.urlPrefix}?filter[Creator][$eq]=${user}`)
         const data = await resp.json()
         return data.data
+    }
+
+    async createArticle(data: PostArticle, token: string): Promise<PostArticle> {
+        const resp = await fetch(`${this.urlPrefix}`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const data_res = await resp.json()
+        return data_res;
     }
 }
--- LetsAsk-frontend/src/repositories/IRepositories.ts
+++ LetsAsk-frontend/src/repositories/IRepositories.ts
@@ -3,7 +3,7 @@ export interface IRepository<A> {
     getAll?() : Promise<A[] | null>;
     getArticleById?(id: string|number) : Promise<A[] | null> 
     getArticleByUser?(user: string) : Promise<A[] | null> 
-
+    createArticle?(data: A, token: string) : Promise<A>;
 
     //Comment
     getCommentByPostId?(id: string|number) : Promise<A[] | null>
