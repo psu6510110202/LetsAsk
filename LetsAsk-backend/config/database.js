b6df941a5893c71b49018c0019d5b356a845e72a
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-02-22 20:42:00 +0700

    Create Docker file and compose

--- LetsAsk-backend/config/database.js
+++ LetsAsk-backend/config/database.js
@@ -4,13 +4,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapiDB'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'secret'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: env('DATABASE_SSL', false)
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA'), // Not required
+      ssl: env.bool('DATABASE_SSL')
     },
     debug: false,
   },
