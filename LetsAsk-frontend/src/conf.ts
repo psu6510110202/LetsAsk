62728131631fef617aa4ac1a7a32dfdeeea2f8cf
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-02-25 02:45:03 +0700

    Change apiPrefix for Strapi

--- LetsAsk-frontend/src/conf.ts
+++ LetsAsk-frontend/src/conf.ts
@@ -2,7 +2,8 @@ const isProd = import.meta.env.PROD;
 
 const conf = {
     isProd,
-    apiPrefix: isProd? 'http://localhost:1337' : 'http://localhost:1337'
+    apiPrefix: isProd? 'https://api.lets-ask.online' : 'http://localhost:1337'
+    // apiPrefix: isProd? 'http://api.lets-ask.online:1337' : 'http://api.lets-ask.online:1337'
 }
 
 export default conf;
